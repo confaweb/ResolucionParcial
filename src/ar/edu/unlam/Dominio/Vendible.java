854277a380128f0eab637cb690c8fbd07664b193
Confalonieri Borojovich Damian Lionel <103534724+confaweb@users.noreply.github.com>
2023-11-11 01:57:22 -0300

    Inicio

---
+++ src/ar/edu/unlam/Dominio/Vendible.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.Dominio;
+
+public interface Vendible {
+	public String getCodigo();
+	public String getNombre();
+	public Double getPrecio();
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ResolucionParcial {
+	requires junit;
+}
